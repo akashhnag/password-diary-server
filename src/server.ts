de6f0fd4eb9afac98a7510994e2f8702f693c2eb
Akash nag <akash.nag@senecaglobal.com>
2022-07-28 16:51:31 +0530

    lint and husky

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express,{Express} from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT =process.env.PORT;
+const { PORT } = process.env;
 
-const app:Express =express();
+const app: Express = express();
 
-app.listen(PORT,()=>{
-console.log(`Server listening at port ${PORT}..`);
-
-})
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}..`);
+});
